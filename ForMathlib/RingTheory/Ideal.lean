ebceac2b1a5460460ee1cad316716d74452aa37f
Antoine Chambert-Loir <antoine.chambert-loir@math.univ-paris-diderot.fr>
2023-07-13 15:09:31 +0200

    the two initial files at the right place

--- ForMathlib/RingTheory/Ideal.lean
+++ ForMathlib/RingTheory/Ideal.lean
@@ -9,19 +9,19 @@ theorem Ideal.image_eq_map_of_surjective {A B : Type _} [Semiring A] [Semiring B
   simp only [SetLike.mem_coe]
   exact Ideal.mem_map_of_mem f ha
   intro b hb
-  simp only [SetLike.mem_coe] at hb 
-  apply Submodule.span_induction hb
+  rw [SetLike.mem_coe, Ideal.map, ← Ideal.submodule_span_eq] at hb 
+  refine' Submodule.span_induction hb _ _ _ _ 
   · exact fun x hx => hx
   · use 0; simp only [SetLike.mem_coe, Submodule.zero_mem, map_zero, eq_self_iff_true, and_self_iff]
   · rintro x y ⟨x, hx, rfl⟩ ⟨y, hy, rfl⟩
     use x + y
     simp only [SetLike.mem_coe] at hx hy ⊢
     simp only [I.add_mem hx hy, map_add, eq_self_iff_true, and_self_iff]
-  · rintro b x ⟨x, hx, rfl⟩
+  · rintro b _ ⟨x, ⟨hx, rfl⟩⟩
     obtain ⟨a, rfl⟩ := hf b
     use a • x
     constructor
-    exact I.smul_mem a (set_like.mem_coe.mp hx)
-    simp only [smul_eq_mul, map_mul]
-#align ideal.image_eq_map_of_surjective Ideal.image_eq_map_of_surjective
+    . exact I.smul_mem a (SetLike.mem_coe.mp hx)
+    . simp only [smul_eq_mul, _root_.map_mul]
+-- #align ideal.image_eq_map_of_surjective Ideal.image_eq_map_of_surjective
 
--- ForMathlib/RingTheory/SubmoduleMem.lean
+++ ForMathlib/RingTheory/SubmoduleMem.lean
@@ -1,30 +1,37 @@
-import Mathbin.RingTheory.Ideal.Operations
+import Mathlib.RingTheory.Ideal.Operations
 
 namespace Submodule
 
 theorem mem_span_iff_exists_sum {R : Type _} [CommSemiring R] {M : Type _} [AddCommMonoid M]
     [Module R M] {ι : Type _} (f : ι → M) (x : M) :
-    x ∈ span R (Set.range f) ↔ ∃ a : ι →₀ R, (a.Sum fun (i : ι) (c : R) => c • f i) = x :=
+    x ∈ span R (Set.range f) ↔ ∃ a : ι →₀ R, (a.sum fun (i : ι) (c : R) => c • f i) = x :=
   by
   rw [← top_smul (span R (Set.range f)), mem_ideal_smul_span_iff_exists_sum]
-  exact exists_congr fun a => ⟨fun ⟨ha, h⟩ => h, fun h => ⟨fun i => mem_top, h⟩⟩
+  exact exists_congr fun a => ⟨fun ⟨_, h⟩ => h, fun h => ⟨fun i => mem_top, h⟩⟩
 #align submodule.mem_span_iff_exists_sum Submodule.mem_span_iff_exists_sum
 
 theorem mem_span_iff_exists_sum' {R : Type _} [CommSemiring R] {M : Type _} [AddCommMonoid M]
     [Module R M] {ι : Type _} (s : Set ι) (f : ι → M) (x : M) :
-    x ∈ span R (f '' s) ↔ ∃ a : ↥s →₀ R, (a.Sum fun (i : ↥s) (c : R) => c • f ↑i) = x :=
+    x ∈ span R (f '' s) ↔ ∃ a : ↥s →₀ R, (a.sum fun (i : ↥s) (c : R) => c • f ↑i) = x :=
   by
   rw [← top_smul (span R (f '' s)), mem_ideal_smul_span_iff_exists_sum']
-  exact exists_congr fun a => ⟨fun ⟨ha, h⟩ => h, fun h => ⟨fun i => mem_top, h⟩⟩
+  apply exists_congr
+  intro a
+  constructor
+  . rintro ⟨_, h⟩
+    exact h
+  . intro h
+    simp only [mem_top, Subtype.forall, implies_true, exists_prop, true_and]
+    exact h
 #align submodule.mem_span_iff_exists_sum' Submodule.mem_span_iff_exists_sum'
 
 theorem mem_span_set_iff_exists_sum {R : Type _} [CommSemiring R] {M : Type _} [AddCommMonoid M]
     [Module R M] (s : Set M) (x : M) :
-    x ∈ span R s ↔ ∃ a : s →₀ R, (a.Sum fun (i : s) (c : R) => c • ↑i) = x :=
+    x ∈ span R s ↔ ∃ a : s →₀ R, ((a.sum fun (i : s) (c : R) => c • (i : M)) = x) :=
   by
   conv_lhs => rw [← Set.image_id s]
   rw [← top_smul (span R (id '' s)), mem_ideal_smul_span_iff_exists_sum']
-  exact exists_congr fun a => ⟨fun ⟨ha, h⟩ => h, fun h => ⟨fun i => mem_top, h⟩⟩
+  exact exists_congr fun a => ⟨fun ⟨_, h⟩ => h, fun h => ⟨fun i => mem_top, h⟩⟩
 #align submodule.mem_span_set_iff_exists_sum Submodule.mem_span_set_iff_exists_sum
 
 end Submodule
