54de1bb1e7ab7f8b4276251604a69bd90e317535
Antoine Chambert-Loir <antoine.chambert-loir@math.univ-paris-diderot.fr>
2023-07-14 12:32:14 +0200

    Ring - bundled version, does not work

--- DividedPowers/Ring.lean
+++ DividedPowers/Ring.lean
@@ -1,22 +1,27 @@
 /- Copyright ACL and MIdFF 
 ! This file was ported from Lean 3 source module divided_powers.ring
 -/
-import Oneshot.DividedPowers.Basic
+import DividedPowers.Basic
+
+/- This would give a bundled version of DividedPower Rings
+but it is presently not consistent with our formalization.
+  Adjust or Delete  -/
+
 
 -- import algebra.ring
 class DividedPowerRing (A : Type _) extends CommRing A where
-  pdIdeal : Ideal A
-  dpow : ℕ → pd_ideal → A
+  dpIdeal : Ideal A
+  dpow : ℕ → dpIdeal → A
   dpow_zero : dpow 0 = 1
   dpow_one : dpow 1 = coe
-  dpow_mem : ∀ (n : ℕ) (x : pd_ideal), 1 ≤ n → dpow n x ∈ pd_ideal
+  dpow_mem : ∀ (n : ℕ) (x : dpIdeal), 1 ≤ n → dpow n x ∈ dpIdeal
   dpow_sum :
-    ∀ (n : ℕ) (x y : pd_ideal),
+    ∀ (n : ℕ) (x y : pdIdeal),
       dpow n (x + y) = Finset.sum (Finset.range (n + 1)) fun k => dpow k x * dpow (n - k) y
-  dpow_smul : ∀ (n : ℕ) (a : A) (x : pd_ideal), dpow n (a • x) = a ^ n * dpow n x
-  dpow_mul : ∀ (m n : ℕ) (x : pd_ideal), dpow m x * dpow n x = Nat.choose (n + m) m * dpow (m + n) x
+  dpow_smul : ∀ (n : ℕ) (a : A) (x : dpIdeal), dpow n (a • x) = a ^ n * dpow n x
+  dpow_mul : ∀ (m n : ℕ) (x : dpIdeal), dpow m x * dpow n x = Nat.choose (n + m) m * dpow (m + n) x
   dpow_comp :
-    ∀ (m n : ℕ) (hn : 1 ≤ n) (x : pd_ideal),
+    ∀ (m n : ℕ) (hn : 1 ≤ n) (x : dpIdeal),
       dpow m ⟨dpow n x, dpow_mem n x hn⟩ = mchoose m n * dpow (m * n) x
 #align divided_power_ring DividedPowerRing
 
