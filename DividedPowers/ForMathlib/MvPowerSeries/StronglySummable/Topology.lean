45426f5b4ae38790c6dcb7ed04b5e0f944ddcf7a
Maria Ines de Frutos Fernandez <mariaines.dff@gmail.com>
2023-08-01 16:10:02 +0200

    finish porting MvPowerSeries.StronglySummable.Topology

--- DividedPowers/ForMathlib/MvPowerSeries/StronglySummable/Topology.lean
+++ DividedPowers/ForMathlib/MvPowerSeries/StronglySummable/Topology.lean
@@ -115,15 +115,12 @@ theorem hasProd_of_one_add (hf : StronglySummable f) :
   intro V hV
   simp only [Filter.mem_map, Filter.mem_atTop_sets, ge_iff_le, Finset.le_eq_subset,
     Set.mem_preimage]
-  set V₀ := Add.add hf.to_stronglyMultipliable.prod ⁻¹' V with V₀_def
+  let V₀ := Add.add hf.to_stronglyMultipliable.prod ⁻¹' V 
   have hV'₀ : V = Add.add (-hf.to_stronglyMultipliable.prod) ⁻¹' V₀ := by
-    simp only [V₀_def, ← Set.preimage_comp, comp_add_left, add_right_neg]
+    simp only [Add.add, ← Set.preimage_comp]
     ext x
-    rw [Set.mem_preimage]
-    simp only [comp_apply]
-    sorry
+    rw [Set.mem_preimage, comp_apply, ← add_assoc, add_right_neg, zero_add]
   have hV₀ : V₀ ∈ nhds (0 : MvPowerSeries σ α) := by
-    simp only [V₀_def]
     apply continuousAt_def.mp (Continuous.continuousAt (continuous_add_left _))
     rw [add_zero]
     exact hV
@@ -135,17 +132,18 @@ theorem hasProd_of_one_add (hf : StronglySummable f) :
   apply htV₀
   simp only [Set.mem_pi, Pi.add_apply, Pi.neg_apply]
   intro d hd
-  convert mem_of_mem_nhds (ht d)
-  simp only [Pi.zero_apply, neg_eq_zero, neg_add_eq_sub, sub_eq_zero]
-  sorry
-  /- convert StronglySummable.Finset.prod_of_one_add_eq hf d J _
+  convert mem_of_mem_nhds (ht d) using 1
+  simp only [Add.add, Pi.sub_apply, neg_add_eq_sub]
+  rw [Pi.zero_apply, sub_eq_zero]
+  convert StronglySummable.Finset.prod_of_one_add_eq hf d J _   
   intro i hi
   apply hIJ
   revert hi
   contrapose
-  simp only [strongly_summable.not_mem_union_of_support_of_coeff_le_iff]
+  simp only [StronglySummable.not_mem_unionOfSupportOfCoeffLe_iff]
   intro h e hed
-  exact h _ (le_trans hed (Finset.le_sup ((Set.Finite.mem_toFinset hD).mpr hd)))  -/
+  refine' h e (le_trans hed _)
+  apply Finset.le_sup ((Set.Finite.mem_toFinset hD).mpr hd)
 #align mv_power_series.strongly_summable.has_prod_of_one_add
   MvPowerSeries.StronglySummable.hasProd_of_one_add
 
@@ -184,4 +182,3 @@ end StronglySummable
 end StronglyMultipliable
 
 end MvPowerSeries
-
