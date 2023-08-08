ecff7a3e270e8d58956d4db5b11b93ceee2d04f7
Maria Ines de Frutos Fernandez <mariaines.dff@gmail.com>
2023-08-08 13:03:44 +0200

    port RobyLemma9

--- DividedPowers/DpAlgebra/RobyLemma9.lean
+++ DividedPowers/DpAlgebra/RobyLemma9.lean
@@ -58,7 +58,8 @@ variable [Algebra R S] [IsScalarTower R S M] [IsScalarTower R S N]
 open Algebra.TensorProduct TensorProduct
 
 def φ : M ⊗[R] N →ₐ[R] M ⊗[S] N :=
-  Algebra.TensorProduct.productMap (Algebra.TensorProduct.includeLeft.restrictScalars R)
+  Algebra.TensorProduct.productMap 
+    ((@Algebra.TensorProduct.includeLeft S _ M  _ _ N _ _ S _ _ _).restrictScalars R)
     (Algebra.TensorProduct.includeRight.restrictScalars R)
 #align φ φ
 
@@ -67,8 +68,7 @@ theorem φ_apply (m : M) (n : N) : φ R S M N (m ⊗ₜ[R] n) = m ⊗ₜ[S] n :=
     includeRight_apply, tmul_mul_tmul, _root_.mul_one, _root_.one_mul]
 #align φ_apply φ_apply
 
-theorem φ_surjective : Function.Surjective (φ R S M N) :=
-  by
+theorem φ_surjective : Function.Surjective (φ R S M N) := by
   intro z
   induction z using TensorProduct.induction_on with
   | C0 => use 0; simp only [map_zero]
@@ -76,35 +76,31 @@ theorem φ_surjective : Function.Surjective (φ R S M N) :=
   | Cp x y hx hy => 
       obtain ⟨a, rfl⟩ := hx
       obtain ⟨b, rfl⟩ := hy
-      use a + b
-      simp only [map_add]
+      exact ⟨a + b, map_add _ _ _⟩
 #align φ_surjective φ_surjective
 
 def kerφ : Ideal (M ⊗[R] N) :=
   Ideal.span ((fun r : S => (r • (1 : M)) ⊗ₜ[R] (1 : N) - (1 : M) ⊗ₜ[R] (r • (1 : N))) '' ⊤)
 #align kerφ kerφ
 
-example : N →ₐ[R] M ⊗[R] N :=
+/- example : N →ₐ[R] M ⊗[R] N :=
   includeRight
 
 example : N →ₐ[R] M ⊗[R] N :=
   includeRight
 
 example : N →ₐ[S] M ⊗[S] N :=
-  includeRight
+  includeRight -/
 
 def ψLeft : M →ₐ[S] M ⊗[R] N ⧸ kerφ R S M N :=
-  {
-    ((Ideal.Quotient.mkₐ S (kerφ R S M N)).restrictScalars R).comp
-      Algebra.TensorProduct.includeLeft with
-    commutes' := fun s =>
-      by
-      simp only [AlgHom.toFun_eq_coe, AlgHom.coe_comp, AlgHom.coe_restrictScalars',
-        Function.comp_apply, includeLeft_apply]
-      simp only [Algebra.algebraMap_eq_smul_one]
-      suffices : (s • (1 : M)) ⊗ₜ[R] (1 : N) = s • (1 : M ⊗[R] N)
+{ ((Ideal.Quotient.mkₐ S (kerφ R S M N)).restrictScalars R).comp
+    Algebra.TensorProduct.includeLeft with
+  commutes' := fun s => by
+    simp only [AlgHom.toFun_eq_coe, AlgHom.coe_comp, AlgHom.coe_restrictScalars',
+      Function.comp_apply, includeLeft_apply, Algebra.algebraMap_eq_smul_one]
+    suffices (s • (1 : M)) ⊗ₜ[R] (1 : N) = s • (1 : M ⊗[R] N) by
       rw [this, AlgHom.map_smul, AlgHom.map_one]
-      rfl }
+    rfl }
 #align ψ_left ψLeft
 
 -- ψ_left' R S M N }
@@ -127,21 +123,19 @@ def ψ : M ⊗[S] N →ₐ[S] M ⊗[R] N ⧸ kerφ R S M N :=
   productMap (ψLeft R S M N) (ψRight R S M N)
 #align ψ ψ
 
-#check Ideal.Quotient.mk
+--#check Ideal.Quotient.mk
 
 theorem ψ_apply (m : M) (n : N) : 
   ψ R S M N (m ⊗ₜ[S] n) = 
-    Ideal.Quotient.mk (kerφ R S M N) (m ⊗ₜ[R] n) :=
-  by
-  simp only [ψ, ψLeft, ψRight]
-  simp only [AlgHom.toRingHom_eq_coe, productMap_apply_tmul, AlgHom.coe_mk, RingHom.coe_coe, AlgHom.coe_comp,
-    AlgHom.coe_restrictScalars', Ideal.Quotient.mkₐ_eq_mk, Function.comp_apply, includeLeft_apply, includeRight_apply]
+    Ideal.Quotient.mk (kerφ R S M N) (m ⊗ₜ[R] n) := by
+  simp only [ψ, ψLeft, ψRight, AlgHom.toRingHom_eq_coe, productMap_apply_tmul, AlgHom.coe_mk, 
+    RingHom.coe_coe, AlgHom.coe_comp, AlgHom.coe_restrictScalars', Ideal.Quotient.mkₐ_eq_mk, 
+    Function.comp_apply, includeLeft_apply, includeRight_apply]
   rw [← RingHom.map_mul]
   simp only [tmul_mul_tmul, _root_.mul_one, _root_.one_mul]
 #align ψ_apply ψ_apply
 
-theorem kerφ_eq : RingHom.ker (φ R S M N).toRingHom = kerφ R S M N :=
-  by
+theorem kerφ_eq : RingHom.ker (φ R S M N).toRingHom = kerφ R S M N := by
   suffices h : kerφ R S M N ≤ RingHom.ker (φ R S M N).toRingHom
   rw [RingHom.ker_eq_ideal_iff]
   use h
@@ -164,11 +158,8 @@ theorem kerφ_eq : RingHom.ker (φ R S M N).toRingHom = kerφ R S M N :=
   intro z hz
   simp only [Set.top_eq_univ, Set.image_univ, Set.mem_range] at hz 
   obtain ⟨r, rfl⟩ := hz
-  simp only [SetLike.mem_coe]
-  simp only [RingHom.sub_mem_ker_iff]
-  simp only [AlgHom.toRingHom_eq_coe, AlgHom.coe_toRingHom]
-  simp only [φ_apply]
-  simp only [TensorProduct.tmul_smul]
+  simp only [SetLike.mem_coe, RingHom.sub_mem_ker_iff,AlgHom.toRingHom_eq_coe, AlgHom.coe_toRingHom,
+    φ_apply, TensorProduct.tmul_smul]
   rfl
 #align kerφ_eq kerφ_eq
 
