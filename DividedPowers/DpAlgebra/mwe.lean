8e95b74b252cef96e92fa40666c8f07e8de03fc8
Antoine Chambert-Loir <antoine.chambert-loir@math.univ-paris-diderot.fr>
2023-08-14 01:50:28 +0200

    upgrade mathlib and the mwe

--- DividedPowers/DpAlgebra/mwe.lean
+++ DividedPowers/DpAlgebra/mwe.lean
@@ -23,26 +23,22 @@ set_option trace.profiler true
 
 def Quot_r := RingQuot (r R M)
 
-instance (priority := 999) : 
+instance : -- (priority := 999) : 
     Semiring (Quot_r R M) := 
   RingQuot.instSemiring _
 
 /- We have to make the priority of CommSemiring very low, 
   or else the `HasQuotient` instance below takes about 3s. -/
-instance (priority := 10) :
+instance : -- (priority := 10) :
     CommSemiring (Quot_r R M) := RingQuot.instCommSemiring _
 
-count_heartbeats in 
-set_option synthInstance.maxHeartbeats 100000 in
-set_option trace.profiler true in 
-set_option pp.proofs.withType false in
 instance {S : Type w} [CommRing S] : CommRing (Quot_r S M) :=
   RingQuot.instCommRingRingQuotToSemiringToCommSemiring _
 
 instance instAlgebra 
     {R A M} [CommSemiring R] [CommRing A] [Algebra R A] :
     Algebra R (Quot_r A M) :=
-  RingQuot.instAlgebraRingQuotInstSemiring _
+  RingQuot.instAlgebraRingQuot _
 
 -- verify there is no diamond
 example (R : Type u) [CommRing R] : 
@@ -51,13 +47,13 @@ example (R : Type u) [CommRing R] :
 
 instance {R S A M} [CommRing R] [CommRing S] [CommRing A]
     [Algebra R A] [Algebra S A] [SMulCommClass R S A] :
-    SMulCommClass R S (Quot_r A M)  :=
-  RingQuot.instSMulCommClassRingQuotInstSMulRingQuotInstSMulRingQuot _
+    SMulCommClass R S (Quot_r A M)  := 
+  RingQuot.instSMulCommClassRingQuot _
 
 instance {R S A M} [CommRing R] [CommRing S] [CommRing A]
     [SMul R S] [Algebra R A] [Algebra S A] [IsScalarTower R S A] :
     IsScalarTower R S  (Quot_r A M) :=
-  RingQuot.instIsScalarTowerRingQuotInstSMulRingQuotInstSMulRingQuot _
+  RingQuot.instIsScalarTowerRingQuot _
 
 instance (R : Type u) [CommRing R] (M : Type v) : 
     HasQuotient (Quot_r R M) (Ideal (Quot_r R M)) := 
