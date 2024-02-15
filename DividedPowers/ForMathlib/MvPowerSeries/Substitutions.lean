d2cf07e203825bf1d85c5fc1aaa42978433e8383
mariainesdff <mariaines.dff@gmail.com>
2024-02-15 14:00:20 +0100

    add lemma statement

--- DividedPowers/ForMathlib/MvPowerSeries/Substitutions.lean
+++ DividedPowers/ForMathlib/MvPowerSeries/Substitutions.lean
@@ -1,9 +1,9 @@
 import DividedPowers.ForMathlib.MvPowerSeries.LinearTopology
 
-/- # Substitutions in power series 
+/- # Substitutions in power series
 
 We follow Bourbaki, Algèbre, chap. 4, §4, n° 3 -/
-/- # Substitutions in power series 
+/- # Substitutions in power series
 
 We follow Bourbaki, Algèbre, chap. 4, §4, n° 3 -/
 variable (α : Type _) [CommRing α]
@@ -26,18 +26,24 @@ example {α β γ : Type _} (φ : α → β) (ψ : β → γ) (f : Filter α) (g
     (hφ : Filter.Tendsto φ f g) (hψ : Filter.Tendsto ψ g h) : Filter.Tendsto (ψ ∘ φ) f h := by
   refine' Filter.Tendsto.comp hψ hφ
 
-theorem Continuous.tendsto_apply_pow_of_constant_coeff_zero (φ : MvPowerSeries σ α →ₐ[α] R)
-    (hφ : Continuous φ) (s : σ) : Filter.Tendsto (fun n : ℕ => φ (X s ^ n)) Filter.atTop (nhds 0) :=
-  by
+/-- Bourbaki, Algèbre, chap. 4, §4, n°3, Prop. 4 (i) (a) -/
+theorem Continuous.tendsto_apply_pow_zero_of_constantCoeff_zero {φ : MvPowerSeries σ α →ₐ[α] R}
+    (hφ : Continuous φ) (s : σ) :
+    Filter.Tendsto (fun n : ℕ => φ (X s ^ n)) Filter.atTop (nhds 0) := by
   rw [← φ.map_zero]
-  refine' Filter.Tendsto.comp hφ.continuousAt (tendsto_pow_of_constantCoeff_zero _)
+  refine' Filter.Tendsto.comp hφ.continuousAt (tendsto_pow_zero_of_constantCoeff_zero _)
   rw [constantCoeff_X]
-#align continuous.tendsto_apply_pow_of_constant_coeff_zero
-  Continuous.tendsto_apply_pow_of_constant_coeff_zero
 
-theorem Continuous.apply_variables (φ : MvPowerSeries σ α →ₐ[α] R) (hφ : Continuous φ) :
+/-- Bourbaki, Algèbre, chap. 4, §4, n°3, Prop. 4 (i) (b) -/
+theorem Continuous.tendsto_apply_variables_zero_of_cofinite {φ : MvPowerSeries σ α →ₐ[α] R}
+    (hφ : Continuous φ) :
     Filter.Tendsto (fun s : σ => φ (X s)) Filter.cofinite (nhds 0) := by
   rw [← φ.map_zero]
   exact Filter.Tendsto.comp hφ.continuousAt variables_tendsto_zero
-#align continuous.apply_variables Continuous.apply_variables
 
+/-- Bourbaki, Algèbre, chap. 4, §4, n°3, Prop. 4 (i) (c) -/
+theorem foo {Y : σ → R}
+    (hY_pow : ∀ s : σ, Filter.Tendsto (fun n : ℕ => (Y s ^ n)) Filter.atTop (nhds 0))
+    (hY_cof : ∀ s : σ, Filter.Tendsto (fun s : σ => (Y s)) Filter.cofinite (nhds 0)) :
+    ∃ (φ : MvPowerSeries σ α →ₐ[α] R) (hφ : Continuous φ), ∀ (s : σ), φ (X s) = Y s := by
+  sorry
