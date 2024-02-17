e19a6030b7a65f1b7767a2f11a01f93b5bf56228
mariainesdff <mariaines.dff@gmail.com>
2024-02-17 12:42:16 +0100

    update DividedPowers.lean

--- DividedPowers.lean
+++ DividedPowers.lean
@@ -1,29 +1,48 @@
-AlgebraComp.lean
-AlgebraLemmas.lean
-Antidiagonal.lean
-BasicLemmas.lean
-CombinatoricsLemmas.lean
-DividedPowers.lean
-GradedAlgebra.lean
-GradedModuleQuot.lean
-GradedRingQuot.lean
-Homogeneous.lean
-TpAlgebra.lean
-WeightedHomogeneous.lean
-lakefile.lean
-MvPowerSeries:
-Topology.lean
-Substitutions.lean
-StronglySummable/
-Order.lean
-LinearTopology.lean
+--import DividedPowers.Basic
+import DividedPowers.BasicLemmas
+--import DividedPowers.DPAlgebra.BaseChange
+--import DividedPowers.DPAlgebra.Dpow
+--import DividedPowers.DPAlgebra.Envelope
+--import DividedPowers.DPAlgebra.Exponential
+--import DividedPowers.DPAlgebra.Graded.Basic
+--import DividedPowers.DPAlgebra.Graded.GradeOne
+--import DividedPowers.DPAlgebra.Graded.GradeZero
+--import DividedPowers.DPAlgebra.Init_copy
+--import DividedPowers.DPAlgebra.Init
+import DividedPowers.DPAlgebra.Misc
+--import DividedPowers.DPAlgebra.mwe
+--import DividedPowers.DPAlgebra.PolynomialMap
+import DividedPowers.DPAlgebra.RobyLemma5
+import DividedPowers.DPAlgebra.RobyLemma9
+--import DividedPowers.DPMorphism
+import DividedPowers.ForMathlib.AlgebraComp
+--import DividedPowers.ForMathlib.AlgebraLemmas
+import DividedPowers.ForMathlib.Antidiagonal
+import DividedPowers.ForMathlib.CombinatoricsLemmas
+import DividedPowers.ForMathlib.GradedAlgebra
+--import DividedPowers.ForMathlib.GradedModuleQuot
+import DividedPowers.ForMathlib.GradedRingQuot
+import DividedPowers.ForMathlib.Homogeneous
+import DividedPowers.ForMathlib.InfiniteSum.Basic
+import DividedPowers.ForMathlib.InfiniteSum.Order
+import DividedPowers.ForMathlib.MvPowerSeries.LinearTopology
+import DividedPowers.ForMathlib.MvPowerSeries.Order
+--import DividedPowers.ForMathlib.MvPowerSeries.StronglySummable.Basic
+--import DividedPowers.ForMathlib.MvPowerSeries.StronglySummable.Topology
+import DividedPowers.ForMathlib.MvPowerSeries.Substitutions
+import DividedPowers.ForMathlib.MvPowerSeries.Topology
+import DividedPowers.ForMathlib.RingTheory.Ideal
+import DividedPowers.ForMathlib.RingTheory.SubmoduleMem
+import DividedPowers.ForMathlib.Topology.LinearTopology
+import DividedPowers.ForMathlib.WeightedHomogeneous
+--import DividedPowers.IdealAdd
+import DividedPowers.PolynomialMap.Basic
+import DividedPowers.PolynomialMap.Coeff
+import DividedPowers.PolynomialMap.Graded
+--import DividedPowers.RatAlgebra
+--import DividedPowers.SubDPIdeal
+#exit
 
-InfiniteSum:
-Order.lean
-Basic.lean
-
-ForMathlib:
-RingTheory/
 
 DividedPowers:
 Topology/
