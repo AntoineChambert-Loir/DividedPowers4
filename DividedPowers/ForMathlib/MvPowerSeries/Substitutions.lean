ad7797ec3bb2b16e2afb541c506dc38c386b9800
Antoine Chambert-Loir <antoine.chambert-loir@math.univ-paris-diderot.fr>
2024-02-28 15:59:54 +0100

    split foo in lemmas and definitions (needs renaming)

--- DividedPowers/ForMathlib/MvPowerSeries/Substitutions.lean
+++ DividedPowers/ForMathlib/MvPowerSeries/Substitutions.lean
@@ -43,80 +43,92 @@ theorem Continuous.tendsto_apply_variables_zero_of_cofinite {φ : MvPowerSeries 
   rw [← φ.map_zero]
   exact Filter.Tendsto.comp hφ.continuousAt variables_tendsto_zero
 
+section foo
 
-
-/-- Bourbaki, Algèbre, chap. 4, §4, n°3, Prop. 4 (ii) - Existence -/
-theorem foo {Y : σ → R}
+variable (Y : σ → R)
     (hY_pow : ∀ s : σ, Filter.Tendsto (fun n : ℕ => (Y s ^ n)) Filter.atTop (nhds 0))
     (hY_cof : ∀ s : σ, Filter.Tendsto (fun s : σ => (Y s)) Filter.cofinite (nhds 0))
     [τ : UniformSpace R] (ι : Type*) [Nonempty ι] [IsLinearTopology R ι] [CompleteSpace R]
-    [T2Space R] :
-    ∃ (φ : MvPowerSeries σ α →ₐ[α] R) (hφ : Continuous φ), ∀ (s : σ), φ (X s) = Y s := by
-  set ψ : MvPolynomial σ α →ₐ[α] R :=
+    [T2Space R]
+
+def foo_ψ : MvPolynomial σ α →ₐ[α] R :=
   { toFun     := fun f => MvPolynomial.aeval Y f
     map_one'  := by simp only [map_one]
     map_mul'  := by simp only [map_mul, forall_const]
     map_zero' := by simp only [map_zero]
     map_add'  := by simp only [map_add, forall_const]
-    commutes' := by simp only [AlgHom.commutes, forall_const] } with hψ_def
-  -- I should move this and di outside the proof
-  letI : TopologicalSpace (MvPolynomial σ α) := TopologicalSpace.induced
+    commutes' := by simp only [AlgHom.commutes, forall_const] }
+
+local instance : TopologicalSpace (MvPolynomial σ α) := TopologicalSpace.induced
     MvPolynomial.toMvPowerSeries (MvPowerSeries.topologicalSpace σ α)
-  haveI di : DenseInducing (@MvPolynomial.toMvPowerSeries σ α _) := {
-    induced := rfl
-    dense   := by
-      rw [DenseRange, Dense]
-      intro f
-      rw [mem_closure_iff]
-      intro S hSopen hSf
-      sorry }
-  have hψ : Continuous ψ := by
+
+def foo_di : DenseInducing (@MvPolynomial.toMvPowerSeries σ α _) := {
+  induced := rfl
+  dense   := by
+    rw [DenseRange, Dense]
+    intro f
+    rw [mem_closure_iff]
+    intro S hSopen hSf
+    sorry }
+
+lemma foo_ψ_continuous : Continuous (foo_ψ α R σ Y) := by
     rw [continuous_def]
     intros U hU
     sorry
-  let φ : MvPowerSeries σ α →ₐ[α] R :=
-  { toFun     := DenseInducing.extend di ψ
-    map_one'  := by rw [← MvPolynomial.coe_one, DenseInducing.extend_eq di hψ, map_one]
+
+noncomputable def foo_φ : MvPowerSeries σ α →ₐ[α] R :=
+  { toFun     := DenseInducing.extend (foo_di α σ) (foo_ψ α R σ Y)
+    map_one'  := by rw [← MvPolynomial.coe_one, DenseInducing.extend_eq (foo_di α σ) (foo_ψ_continuous α R σ Y), map_one]
     map_mul'  := by
       simp only [AlgHom.mk_coe]
       sorry
     map_zero' := by
       rw [← MvPolynomial.coe_zero]
       simp only [AlgHom.mk_coe]
-      erw [DenseInducing.extend_eq di hψ, map_zero]
+      erw [DenseInducing.extend_eq (foo_di α σ) (foo_ψ_continuous α R σ Y), map_zero]
     map_add'  := sorry
     commutes' := sorry }
-  have : φ = DenseInducing.extend di ψ := rfl
-  have hφ : Continuous φ := by
-    rw [this]
-    apply DenseInducing.continuous_extend di
-    intro f
-    use (φ f)
-    rw [tendsto_nhds]
-    intros U hUopen hUf
-    rw [Filter.mem_comap]
-    use (φ ⁻¹' U)
-    constructor
-    · rw [mem_nhds_iff]
-      have := MvPolynomial.toMvPowerSeries ⁻¹' (⇑φ ⁻¹' U)
-      sorry
-    · intro P hP
-      change DenseInducing.extend di ⇑(ψ) ↑P ∈ U at hP
-      rw [DenseInducing.extend_eq di hψ] at hP
-      exact hP
-  use φ, hφ
-  intro s
-  rw [this, ← MvPolynomial.coe_X, DenseInducing.extend_eq di hψ (MvPolynomial.X s)]
-  simp only [AlgHom.mk_coe, MvPolynomial.aeval_X]
+
+theorem foo_φ_coe :
+    foo_φ α R σ Y = DenseInducing.extend (foo_di α σ)  (foo_ψ α R σ Y) := rfl
+
+theorem foo_Continuous_φ : Continuous (foo_φ α R σ Y) := by
+  rw [foo_φ_coe]
+  apply DenseInducing.continuous_extend (foo_di α σ)
+  intro f
+  use (foo_φ α R σ Y f)
+  rw [tendsto_nhds]
+  intros U hUopen hUf
+  rw [Filter.mem_comap]
+  use (foo_φ α R σ Y ⁻¹' U)
+  constructor
+  · rw [mem_nhds_iff]
+    have := MvPolynomial.toMvPowerSeries ⁻¹' (⇑(foo_φ α R σ Y) ⁻¹' U)
+    sorry
+  · intro P hP
+    rw [Set.mem_preimage, foo_φ_coe, Set.mem_preimage,
+      DenseInducing.extend_eq _ (foo_ψ_continuous α R σ Y)] at hP
+    exact hP
+
+theorem foo_φ_apply_X (s : σ) : (foo_φ α R σ Y) (X s) = Y s := by
+  rw [foo_φ_coe, ← MvPolynomial.coe_X,
+    DenseInducing.extend_eq _ (foo_ψ_continuous _ _ _ _ ) (MvPolynomial.X s)]
+  simp only [foo_ψ, AlgHom.mk_coe, MvPolynomial.aeval_X]
+
+variable {Y}
+
+/-- Bourbaki, Algèbre, chap. 4, §4, n°3, Prop. 4 (ii) - Existence -/
+theorem foo :
+    ∃ (φ : MvPowerSeries σ α →ₐ[α] R) (_ : Continuous φ), ∀ (s : σ), φ (X s) = Y s := by
+  use foo_φ α R σ Y, foo_Continuous_φ α R σ Y, fun s ↦ foo_φ_apply_X α R σ Y s
+
 
 /-- Bourbaki, Algèbre, chap. 4, §4, n°3, Prop. 4 (ii) - uniqueness -/
-theorem foo_unique {Y : σ → R}
-    (hY_pow : ∀ s : σ, Filter.Tendsto (fun n : ℕ => (Y s ^ n)) Filter.atTop (nhds 0))
-    (hY_cof : ∀ s : σ, Filter.Tendsto (fun s : σ => (Y s)) Filter.cofinite (nhds 0))
-    [τ : UniformSpace R] (ι : Type*) [Nonempty ι] [IsLinearTopology R ι] [CompleteSpace R]
-    [T2Space R] (φ : MvPowerSeries σ α →ₐ[α] R) (hφ : Continuous φ)
-      (hφ' : ∀ (s : σ), φ (X s) = Y s) :
-    φ = (foo α R σ hY_pow hY_cof ι).choose := by
+theorem foo_unique (φ : MvPowerSeries σ α →ₐ[α] R) (hφ : Continuous φ)
+      (hφ' : ∀ (s : σ), φ (X s) = Y s) (f : MvPowerSeries σ α) :
+    φ f = foo_φ α R σ Y f := by
+  sorry
+ /-
   set ψ := (foo α R σ hY_pow hY_cof ι).choose with hψ
   letI : TopologicalSpace (MvPolynomial σ α) := TopologicalSpace.induced
     MvPolynomial.toMvPowerSeries (MvPowerSeries.topologicalSpace σ α)
@@ -138,6 +150,7 @@ theorem foo_unique {Y : σ → R}
         (foo α R σ hY_pow hY_cof ι).choose_spec.2]
   ext x
   rw [← DenseInducing.extend_unique di hφψ hφ]
+-/
 
 
-  sorry
+end foo
