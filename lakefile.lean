2a100cd3bf83caa6be14c5ff176b2e2ee5ba80e2
mariainesdff <mariaines.dff@gmail.com>
2024-04-02 11:16:22 +0200

    update lakefile.lean

--- lakefile.lean
+++ lakefile.lean
@@ -1,14 +1,36 @@
 import Lake
 open Lake DSL
 
+def moreServerArgs := #[
+  "-Dpp.unicode.fun=true", -- pretty-prints `fun a ↦ b`
+  "-DautoImplicit=false",
+  "-DrelaxedAutoImplicit=false"
+]
+
+-- These settings only apply during `lake build`, but not in VSCode editor.
+def moreLeanArgs := moreServerArgs
+
+-- These are additional settings which do not affect the lake hash,
+-- so they can be enabled in CI and disabled locally or vice versa.
+-- Warning: Do not put any options here that actually change the olean files,
+-- or inconsistent behavior may result
+def weakLeanArgs : Array String :=
+  if get_config? CI |>.isSome then
+    #["-DwarningAsError=true"]
+  else
+    #[]
+
 package «dividedPowers» {
-  -- add any package configuration options here
+leanOptions := #[
+    ⟨`relaxedAutoImplicit, false⟩, -- prevents typos to be interpreted as new free variables
+    ⟨`pp.unicode.fun, true⟩, -- pretty-prints `fun a ↦ b`
+    ⟨`autoImplicit, false⟩]
 }
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
 
 @[default_target]
-lean_lib «DividedPowers» {
-  -- add any library configuration options here
-}
+lean_lib «DividedPowers» where
+  moreLeanArgs := moreLeanArgs
+  weakLeanArgs := weakLeanArgs
