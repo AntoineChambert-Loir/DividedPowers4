94f27e0215f0d7566af43215c47c4bd5bb5d5c64
mariainesdff <mariaines.dff@gmail.com>
2024-04-11 15:05:23 +0200

    update DividedPowers.lean

--- DividedPowers.lean
+++ DividedPowers.lean
@@ -9,7 +9,7 @@ import DividedPowers.DPAlgebra.Graded.GradeOne
 import DividedPowers.DPAlgebra.Graded.GradeZero
 import DividedPowers.DPAlgebra.Init
 import DividedPowers.DPAlgebra.Misc
---import DividedPowers.DPAlgebra.PolynomialMap -- It doesn't work, relies on missing import
+--import DividedPowers.DPAlgebra.PolynomialMap
 import DividedPowers.DPAlgebra.RobyLemma9
 import DividedPowers.DPMorphism
 --import DividedPowers.ForMathlib.AlgebraComp -- This file has errors, but I think we should not use it
@@ -22,20 +22,27 @@ import DividedPowers.ForMathlib.GradedRingQuot
 import DividedPowers.ForMathlib.Homogeneous
 --import DividedPowers.ForMathlib.InfiniteSum.Basic -- In mathlib
 --import DividedPowers.ForMathlib.InfiniteSum.Order -- In mathlib
+import DividedPowers.ForMathlib.Lcoeff
+--import DividedPowers.ForMathlib.LinearEquiv -- Delete it and remove the import from other files
 import DividedPowers.ForMathlib.MvPowerSeries.Basic
+import DividedPowers.ForMathlib.MvPowerSeries.Evaluation
+import DividedPowers.ForMathlib.MvPowerSeries.Exponential
 import DividedPowers.ForMathlib.MvPowerSeries.LinearTopology
 import DividedPowers.ForMathlib.MvPowerSeries.Order
+--import DividedPowers.ForMathlib.MvPowerSeries.Sandbox
 --import DividedPowers.ForMathlib.MvPowerSeries.StronglySummable.Basic -- It has errors related to antidiagonal
 --import DividedPowers.ForMathlib.MvPowerSeries.StronglySummable.Topology
 import DividedPowers.ForMathlib.MvPowerSeries.Substitutions -- It has sorrys
 import DividedPowers.ForMathlib.MvPowerSeries.Topology
+import DividedPowers.ForMathlib.PowerSeries.Topology
 import DividedPowers.ForMathlib.RingTheory.Ideal
 import DividedPowers.ForMathlib.RingTheory.SubmoduleMem
---import DividedPowers.ForMathlib.RingTheory.TensorProduct.Polynomial -- It doesn't work, relies on missing import
+--import DividedPowers.ForMathlib.RingTheory.TensorProduct.MvPolynomial
+--import DividedPowers.ForMathlib.RingTheory.TensorProduct.Polynomial
+import DividedPowers.ForMathlib.Topology.Algebra.Algebra.Basic
 import DividedPowers.ForMathlib.Topology.LinearTopology -- It has sorrys
 import DividedPowers.ForMathlib.WeightedHomogeneous
 import DividedPowers.IdealAdd -- It uses sorry
--- The next two files rely on a missing import DividedPowers.ForMathlib.LinearEquiv
 --import DividedPowers.PolynomialMap.Basic -- It has sorrys
 --import DividedPowers.PolynomialMap.Coeff
 --import DividedPowers.PolynomialMap.Homogeneous
