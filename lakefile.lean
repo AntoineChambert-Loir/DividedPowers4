fd1e5fa170bd2940bd1d81d4511a3da4945b0c11
mariainesdff <mariaines.dff@gmail.com>
2024-05-15 16:01:01 +0200

    Setup blueprint

--- lakefile.lean
+++ lakefile.lean
@@ -34,3 +34,9 @@ require mathlib from git
 lean_lib «DividedPowers» where
   moreLeanArgs := moreLeanArgs
   weakLeanArgs := weakLeanArgs
+
+require checkdecls from git "https://github.com/PatrickMassot/checkdecls.git"
+
+meta if get_config? env = some "dev" then
+require «doc-gen4» from git
+  "https://github.com/leanprover/doc-gen4" @ "main"
