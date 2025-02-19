2c382adc4fe87dbce07808bdc823e8740934083e
mariainesdff <mariaines.dff@gmail.com>
2025-02-19 11:18:28 +0100

    fix Exponential.lean

--- DividedPowers/Exponential.lean
+++ DividedPowers/Exponential.lean
@@ -19,13 +19,13 @@ theorem dpowExp_isExponential (hI : DividedPowers I) {a : R} (ha : a ∈ I) :
   rw [isExponential_iff]
   constructor
   · intro p q
-    simp only [dpowExp, coeff_mk, hI.dpow_mul ha]
+    simp only [dpowExp, coeff_mk, hI.dpow_mul _ _ ha]
   · simp only [dpowExp, ← coeff_zero_eq_constantCoeff_apply, coeff_mk, dpow_zero _ ha]
 
 lemma dpowExp_smul (hI : DividedPowers I) {r a : R} (ha : a ∈ I) :
     hI.dpowExp (r * a) = scale r (hI.dpowExp a) := by
   ext n
-  simp only [dpowExp, coeff_mk, hI.dpow_smul ha, coeff_scale, smul_eq_mul]
+  simp only [dpowExp, coeff_mk, hI.dpow_smul _ ha, coeff_scale, smul_eq_mul]
 
 /-- The power series `hI.dpowExp a`, as an element of `ExponentialModule R`. -/
 def exp (hI : DividedPowers I) (a : I) : ExponentialModule R :=
@@ -57,10 +57,10 @@ noncomputable def ofExp  [DecidablePred (fun x ↦ x ∈ I)] (e : I →ₗ[R] Ex
       = Nat.uniformBell m n * coeff R (m * n) (e a)) :
     DividedPowers I where
   dpow n a       := if ha : a ∈ I then coeff R n (toPowerSeries (e ⟨a, ha⟩)) else 0
-  dpow_null ha   := by simp only [dif_neg ha]
+  dpow_null _ _ ha   := by simp only [dif_neg ha]
   dpow_zero ha   := by simp only [dif_pos ha, coeff_zero_eq_constantCoeff_apply, constantCoeff_coe]
   dpow_one ha    := by simp only [dif_pos ha, coeff_one]
-  dpow_mem hn ha := by simp only [dif_pos ha]; exact coeff_mem _ hn
+  dpow_mem _ _ hn ha := by simp only [dif_pos ha]; exact coeff_mem _ hn
   dpow_add n {a b} ha hb := by
     simp only [dif_pos (I.add_mem ha hb), dif_pos ha, dif_pos hb]
     have : e ⟨a + b, I.add_mem ha hb⟩ = e ⟨a, ha⟩ + e ⟨b, hb⟩ := by rw [← LinearMap.map_add]; rfl
@@ -70,8 +70,8 @@ noncomputable def ofExp  [DecidablePred (fun x ↦ x ∈ I)] (e : I →ₗ[R] Ex
     simp only [dif_pos ha, dif_pos (I.mul_mem_left r ha)]
     have : (⟨r * a, I.mul_mem_left r ha⟩ : I) = r • ⟨a, ha⟩ := rfl
     rw [this, map_smul, coe_smul, coeff_scale, smul_eq_mul]
-  dpow_mul ha := by simp only [dif_pos ha, add_mul_coe']
-  dpow_comp hn ha := by simp only [dif_pos ha, dif_pos (coeff_mem _ hn)]; exact coeff_comp _ _ hn
+  dpow_mul _ _ _ ha := by simp only [dif_pos ha, add_mul_coe']
+  dpow_comp _ _ _ hn ha := by simp only [dif_pos ha, dif_pos (coeff_mem _ hn)]; exact coeff_comp _ _ hn
 
 
 end DividedPowers
