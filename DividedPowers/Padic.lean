1fc9acfb0ac3bf051b4154fbc74c017bd8a8b7b3
mariainesdff <mariaines.dff@gmail.com>
2025-03-11 16:12:49 +0100

    fix comment

--- DividedPowers/Padic.lean
+++ DividedPowers/Padic.lean
@@ -4,12 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Antoine Chambert-Loir, María Inés de Frutos-Fernández
 -/
 
-/-
-Copyright (c) 2022 Antoine Chambert-Loir, María Inés de Frutos-Fernández. All rights reserved.
-Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Antoine Chambert-Loir, María Inés de Frutos-Fernández
--/
-
 import DividedPowers.RatAlgebra
 import Mathlib.NumberTheory.Padics.PadicIntegers
 
