ab2e4cf5133ee23db575fdc14fddb787f0261233
Antoine Chambert-Loir <antoine.chambert-loir@u-paris.fr>
2025-08-23 17:22:13 +0200

    some work on base change

--- DividedPowers/PolynomialLaw/BaseChange.lean
+++ DividedPowers/PolynomialLaw/BaseChange.lean
@@ -1,85 +1,133 @@
 import DividedPowers.PolynomialLaw.Basic2
 
-/-
+/-! # Base change of polynomial maps
 
-# Base Change
-
-The goal is to define a base change map
+This file defines a base change map
   `PolynomialLaw R M N →ₗ[R] PolynomialLaw R' (R' ⊗[R] M) (R' ⊗[R] N)``
 when M and N are R-modules and R' is an R-algebra (commutative)
+
+## Work in progress
 
 This requires to simplify the tensor product
   `S' ⊗[R'] (R' ⊗[R] M)`
 to
   `S' ⊗[R] M`
+using an S'-isomorphism — something Mathlib doesn't know (yet)
+because TensorProduct.assoc lacks some heteronomy.
+This is done in a first part.
 
-an S'-isomorphism which Mathlib doesn't know (yet)
+Then the base change is defined as a map.
 
-What follows is draft
+## TODO
 
-Prove that coeff, multiCoeff, biCoeff commute with base change
-
-Compare ground
-
-(Maybe) commSemiring the file
+- Compare ground
+- Extend `PolynomialMap.baseChange` to a linear map.
+- Prove that coeff, multiCoeff, biCoeff, commute with base change,
+as well as for divided (partial) differentials
 
 -/
+
+namespace TensorProduct
+
+variable {R : Type*} [CommSemiring R]
+  {M : Type*}  [AddCommMonoid M] [Module R M]
+
+variable {R' : Type*} [CommSemiring R'] [Algebra R R']
+variable {S' : Type*} [CommSemiring S'] [Algebra R' S']
+variable {S'' : Type*} [CommSemiring S''] [Algebra R' S'']
+variable (φ : S' →ₐ[R'] S'')
+
+variable [Algebra R S'] [IsScalarTower R R' S']
+variable [Algebra R S''] [IsScalarTower R R' S'']
+
+/-- an auxiliary map -/
+def baseChangeEquiv' : (S' ⊗[R'] R') ⊗[R] M ≃ₗ[S'] S' ⊗[R] M := {
+  toAddEquiv := (LinearEquiv.rTensor M ((TensorProduct.rid R' S').restrictScalars R)).toAddEquiv
+  map_smul' r' srm := by
+    induction srm using TensorProduct.induction_on with
+    | zero => simp
+    | add x y hx hy =>
+      simp only [LinearEquiv.coe_rTensor, smul_add, AddHom.toFun_eq_coe, map_add,
+        LinearMap.coe_toAddHom, RingHom.id_apply]
+      congr
+    | tmul sr m =>
+      simp
+      rw [smul_tmul']
+      simp
+      congr
+      induction sr using TensorProduct.induction_on with
+      | zero => simp
+      | add x y hx hy => simp only [smul_add, map_add, hx, hy]
+      | tmul s r =>
+        rw [← LinearEquiv.eq_symm_apply]
+        simp only [rid_tmul, smul_eq_mul, Algebra.mul_smul_comm, map_smul, rid_symm_apply]
+        rw [smul_tmul', smul_eq_mul]
+        simp [← tmul_smul] }
+
+def baseChangeEquiv :
+    S' ⊗[R'] R' ⊗[R] M ≃ₗ[S'] S' ⊗[R] M :=
+  (AlgebraTensorModule.assoc R R' S' S' R' M).symm.trans baseChangeEquiv'
+
+theorem baseChangeEquiv_rTensor_baseChangeEquivSymm (y : S' ⊗[R] M) :
+    baseChangeEquiv
+      ((LinearMap.rTensor (R' ⊗[R] M) φ.toLinearMap) ((baseChangeEquiv (R' := R')).symm y)) =
+      LinearMap.rTensor M ((φ.restrictScalars R).toLinearMap) y := by
+  induction y using TensorProduct.induction_on with
+  | zero => simp
+  | add x y hx hy => simp only [map_add, hx, hy]
+  | tmul s n => simp [baseChangeEquiv, baseChangeEquiv']
+
+theorem baseChangeEquiv_rTensor_eq_baseChangeEquivSymm_rTensor (x : S' ⊗[R'] R' ⊗[R] M) :
+    baseChangeEquiv ((LinearMap.rTensor (R' ⊗[R] M) φ.toLinearMap) x) =
+      LinearMap.rTensor M ((φ.restrictScalars R).toLinearMap) (baseChangeEquiv x) := by
+  set y := baseChangeEquiv x with hy
+  rw [← LinearEquiv.eq_symm_apply] at hy
+  rw [hy, baseChangeEquiv_rTensor_baseChangeEquivSymm]
+
+end TensorProduct
+
+namespace PolynomialLaw
 
 open TensorProduct
 
 universe u v w
-variable {R : Type u} [CommRing R]
-  {M : Type*}  [AddCommGroup M] [Module R M]
-  {N : Type*} [AddCommGroup N] [Module R N]
+variable {R : Type u} [CommSemiring R]
+  {M : Type*}  [AddCommMonoid M] [Module R M]
+  {N : Type*} [AddCommMonoid N] [Module R N]
 
-variable {R' : Type v} [CommRing R'] [Algebra R R']
-variable {S' : Type w} [CommRing S'] [Algebra R' S']
+variable (R' : Type v) [CommSemiring R'] [Algebra R R']
 
-variable [Algebra R S'] [IsScalarTower R R' S']
+/-- The base change of a polynomial law. -/
+noncomputable def baseChange (f : M →ₚₗ[R] N) :
+    (R' ⊗[R] M) →ₚₗ[R'] (R' ⊗[R] N) where
+  toFun' S' _ _ srm := by
+    let _ : Algebra R S' := RingHom.toAlgebra ((algebraMap R' S').comp (algebraMap R R'))
+    have _ : IsScalarTower R R' S' := IsScalarTower.of_algebraMap_eq
+      (fun r ↦ by simp [RingHom.algebraMap_toAlgebra])
+    exact baseChangeEquiv.symm (f.toFun S' (baseChangeEquiv srm))
+  isCompat' {S' _ _ S'' _ _} φ := by
+    let _ : Algebra R S' := RingHom.toAlgebra ((algebraMap R' S').comp (algebraMap R R'))
+    have _ : IsScalarTower R R' S' := IsScalarTower.of_algebraMap_eq
+      (fun r ↦ by simp [RingHom.algebraMap_toAlgebra])
+    let _ : Algebra R S'' := RingHom.toAlgebra ((algebraMap R' S'').comp (algebraMap R R'))
+    have _ : IsScalarTower R R' S'' := IsScalarTower.of_algebraMap_eq
+      (fun r ↦ by simp [RingHom.algebraMap_toAlgebra])
+    ext srm
+    dsimp only
+    symm
+    simp only [Function.comp_apply]
+    rw [LinearEquiv.symm_apply_eq]
+    rw [baseChangeEquiv_rTensor_eq_baseChangeEquivSymm_rTensor]
+    rw [baseChangeEquiv_rTensor_baseChangeEquivSymm]
+    rw [← f.isCompat_apply]
 
--- variant of `Algebra.TensorProdyct.includeRight`
-noncomputable def baseChange_include : M →ₗ[R] R' ⊗[R] M := {
-  toFun     := fun m ↦ 1 ⊗ₜ[R] m
-  map_add'  := fun x y ↦ TensorProduct.tmul_add 1 x y
-  map_smul' := fun r m  ↦ by
-    simp only [AddHom.toFun_eq_coe, AddHom.coe_mk, RingHom.id_apply, ← TensorProduct.smul_tmul]
-    rfl }
+def baseChange_linearMap : (M →ₚₗ[R] N) →ₗ[R] RestrictScalars R R' ((R' ⊗[R] M) →ₚₗ[R'] (R' ⊗[R] N)) where
+  toFun := baseChange R'
+  map_add' f g := sorry
+  map_smul' r f := sorry
 
--- Try to use some module analogue of Algebra.TensorProduct.assoc and Algebra.TensorProduct.rid.
-
-example : S' ⊗[R'] (R' ⊗[R] M) →ₗ[S'] S' ⊗[R] M := by
-  apply TensorProduct.AlgebraTensorModule.lift {
-    toFun := fun s' ↦ {
-      toFun := TensorProduct.lift {
-        toFun := fun r' ↦ r' • baseChange_include
-        map_add' := fun a b ↦ by simp only [add_smul]
-        map_smul' := fun a b ↦ by
-          simp only [AddHom.toFun_eq_coe, AddHom.coe_mk, RingHom.id_apply, smul_assoc] }
-      map_add' := fun x y ↦ by rw [map_add]
-      map_smul' := fun r x ↦ by
-        simp only [RingHom.id_apply]
-        sorry }
-    map_add'  := fun x y ↦ by
-      sorry
-    map_smul' := sorry
-  }
-
-example : S' ⊗[R'] (R' ⊗[R] M) ≃ₗ[S'] S' ⊗[R] M := by
+theorem baseChange_ground (f : M →ₚₗ[R] N) :
+    (f.baseChange R').ground = f.toFun R' := by
   sorry
 
---Universe error
-def baseChange (f : PolynomialLaw R M N) : PolynomialLaw R' (R' ⊗[R] M) (R' ⊗[R] N) where
-  toFun' S' _ _ := by
-    /- have : Algebra R S' := RingHom.toAlgebra ((algebraMap R' S').comp (algebraMap R R'))
-    let fS' := toFun' f S'
-    let u := Algebra.TensorProduct.rid R' S' -/
-
-    sorry
-  isCompat' := sorry
-
-end BaseChange
-
-
 end PolynomialLaw
-
-end
