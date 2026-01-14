6c5a97ace19241492f6640e638a59ed2d63f6fa0
Antoine Chambert-Loir <antoine.chambert-loir@u-paris.fr>
2026-01-14 13:38:54 +0100

    adjust ForMathlib.BigOperators.Basic

--- DividedPowers/PolynomialLaw/Homogeneous.lean
+++ DividedPowers/PolynomialLaw/Homogeneous.lean
@@ -3,7 +3,7 @@ Copyright (c) 2025 Antoine Chambert-Loir, María Inés de Frutos-Fernández. All
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Antoine Chambert-Loir, María Inés de Frutos-Fernández
 -/
-import DividedPowers.ForMathlib.Algebra.BigOperators.Finsupp.Basic
+import Mathlib.Algebra.BigOperators.Finsupp.Basic
 import DividedPowers.ForMathlib.Algebra.Polynomial.AlgebraMap
 import DividedPowers.ForMathlib.RingTheory.TensorProduct.Polynomial
 import DividedPowers.PolynomialLaw.Coeff
