1fb9217cec18c4d1a000ff9c4c8b86b085404647
Antoine Chambert-Loir <antoine.chambert-loir@u-paris.fr>
2026-01-14 13:34:12 +0100

    update DividedPowers.lean

--- DividedPowers.lean
+++ DividedPowers.lean
@@ -22,7 +22,7 @@ import DividedPowers.ForMathlib.Algebra.Algebra.Bilinear
 import DividedPowers.ForMathlib.Algebra.BigOperators.Finsupp.Basic -- In PR #30537
 -- import DividedPowers.ForMathlib.Algebra.BigOperators.Finsupp.Fin -- In PR #30538
 import DividedPowers.ForMathlib.Algebra.BigOperators.Group.Finset.Basic -- In PR #30539
-import DividedPowers.ForMathlib.Algebra.Module.LinearMap.Defs -- In PR #30546
+-- import DividedPowers.ForMathlib.Algebra.Module.LinearMap.Defs -- In PR #30546
 --import DividedPowers.ForMathlib.Algebra.MvPolynomial.Equiv -- In PR #15019
 import DividedPowers.ForMathlib.Algebra.MvPolynomial.Lemmas
 import DividedPowers.ForMathlib.Algebra.Polynomial.AlgebraMap -- In PR #30547
