7604bb98f9fdea095514806299a89b68420b3d70
mariainesdff <mariaines.dff@gmail.com>
2026-02-25 16:21:40 +0100

    PR #35756

--- DividedPowers/ForMathlib/Data/Nat/Choose/Multinomial.lean
+++ DividedPowers/ForMathlib/Data/Nat/Choose/Multinomial.lean
@@ -11,6 +11,8 @@ We provide some lemmas for manipulating multinomial coefficients.
 
 -/
 
+-- In #35756
+
 section
 
 namespace Nat
@@ -18,38 +20,27 @@ namespace Nat
 open Finset
 
 theorem multinomial_congr_of_sdiff {α : Type*} [DecidableEq α] {f g : α → ℕ} {s t : Finset α}
-    (hst : s ⊆ t) (H1 : ∀ a ∈ t \ s, g a = 0) (H2 : ∀ a ∈ s, f a = g a) :
+    (hst : s ⊆ t) (hf : ∀ a ∈ t \ s, g a = 0) (hg : ∀ a ∈ s, f a = g a) :
     multinomial s f = multinomial t g := by
-  rw [← Nat.mul_right_inj (a := ∏ a ∈ t, (g a)!), multinomial_spec,
-    ← sum_subset_zero_on_sdiff hst H1 H2, ← multinomial_spec s f]
-  · apply congr_arg₂ _ _ rfl
-    symm
-    apply prod_subset_one_on_sdiff hst
-    · intro x hx
-      rw [H1 x hx, factorial_zero]
-    · intro x hx
-      rw [H2 x hx]
-  · simp only [ne_eq, prod_eq_zero_iff, not_exists, not_and]
-    intro x hx
-    exact factorial_ne_zero (g x)
+  rw [← Nat.mul_right_inj (prod_ne_zero_iff.mpr (fun x _ ↦ factorial_ne_zero (g x))),
+    multinomial_spec, ← sum_subset_zero_on_sdiff hst hf hg, ← multinomial_spec s f]
+  apply congr_arg₂ _ _ rfl
+  symm
+  apply prod_subset_one_on_sdiff hst
+  · intro x hx
+    rw [hf x hx, factorial_zero]
+  · intro x hx
+    rw [hg x hx]
 
 theorem multinomial_single {α : Type*} [DecidableEq α] (s : Finset α) (a : α) (n : ℕ) :
     multinomial s (Pi.single a n) = 1 := by
-  rw [← Nat.mul_right_inj, mul_one, multinomial_spec]
-  · simp only [sum_pi_single']
-    split_ifs with ha
-    · rw [Finset.prod_eq_single a]
-      · simp
-      · intro b hb hba
-        simp [Pi.single_apply, if_neg hba]
-      · simp_all
-    · rw [eq_comm, factorial_zero]
-      apply Finset.prod_eq_one
-      intro b hb
-      rw [Pi.single_apply, if_neg, factorial_zero]
-      exact ne_of_mem_of_not_mem hb ha
-  · simp only [ne_eq, prod_eq_zero_iff, not_exists, not_and]
-    intro x hx
-    apply factorial_ne_zero
+  rw [← Nat.mul_right_inj (prod_ne_zero_iff.mpr (fun _ _ ↦ factorial_ne_zero _)), mul_one,
+    multinomial_spec, sum_pi_single']
+  split_ifs with ha
+  · rw [Finset.prod_eq_single a (by simp_all) (by simp_all), Pi.single_eq_same]
+  · rw [eq_comm, factorial_zero]
+    apply Finset.prod_eq_one
+    intro _ hb
+    rw [Pi.single_apply, if_neg (ne_of_mem_of_not_mem hb ha), factorial_zero]
 
 end Nat
